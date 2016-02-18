ccfbad8410fa653109e453b4582e812ba15b51e4
SdtBarbarossa <DaMicha_essen@gmx.de>
2016-02-18 18:22:37 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
 
 ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
-ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
       include $(call all-subdir-makefiles)
